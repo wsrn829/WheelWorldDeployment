4ecfaef913b44e766908fe8e123d7da576f59b9f
wsrn829 <wsrn829@gmail.com>
2024-04-21 00:52:43 -0400

    Update

--- network/models.py
+++ network/models.py
@@ -5,34 +5,16 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, validators=[RegexValidator(r'^[a-zA-Z ]+$')])
-    date_of_birth = models.DateField()
-    about = models.TextField(blank=True)
-    country = models.CharField(max_length=100, blank=True, validators=[RegexValidator(r'^[a-zA-Z ]+$')])
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-    follows = models.ManyToManyField('self', related_name='followers', symmetrical=False)
-
-    def __str__(self):
-        return self.user.username
-
 class Post(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='posts')
-    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    like = models.ManyToManyField(User, blank=True, related_name='like', )
 
     class Meta:
         ordering = ['-created_at']
 
-class Comment(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_comments')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Like(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follows')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
--- network/urls.py
+++ network/urls.py
@@ -1,11 +1,18 @@
-
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("new_post", views.new_post, name="new_post"),
+    path("posts", views.posts, name="posts"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("follow/<int:user_id>", views.follow, name="follow"),
+    path("unfollow/<int:user_id>", views.unfollow, name="unfollow"),
+    path("edit_post/<int:post_id>", views.edit_post, name="edit_post"),
+    path("like_post/<int:post_id>", views.like_post, name="like_post"),
+    path("following_posts", views.following_posts, name="following_posts"),
+    path("following", views.following, name="following"),
 ]
