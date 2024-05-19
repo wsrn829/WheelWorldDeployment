627362eb2392aa75ccc7decb43e856264fb3e048
wsrn829 <wsrn829@gmail.com>
2024-05-18 22:12:41 -0400

    Add mobile responsiveness

--- ghi/src/ManufacturerForm.js
+++ ghi/src/ManufacturerForm.js
@@ -39,25 +39,25 @@ export default function ManufacturerForm() {
   }
 
   return (
-    <div>
-      <div className="my-5 container">
-        <div className="row">
-          <div className="offset-3 col-6">
-            <div className="shadow p-4 mt-4" style={{ backgroundColor: '#f2f2f2' }}>
-              <h1>Add a manufacturer</h1>
-              <form onSubmit={handleSubmit} id="create-salesperson-form">
-                <div className="form-floating mb-3">
-                  <input
-                    autoFocus value={name} onChange={handleName} placeholder="Name" required type="text" name="name" id="name" className="form-control"
-                  />
-                  <label htmlFor="name">Manufacturer Name</label>
-                </div>
-                <button className="btn btn-primary">Create</button>
-              </form>
+<div>
+  <div className="my-5 container">
+    <div className="row justify-content-center">
+      <div className="col-12 col-sm-8 col-md-6">
+        <div className="shadow p-4 mt-4" style={{ backgroundColor: '#f2f2f2' }}>
+          <h1>Add a manufacturer</h1>
+          <form onSubmit={handleSubmit} id="create-salesperson-form">
+            <div className="form-floating mb-3">
+              <input
+                autoFocus value={name} onChange={handleName} placeholder="Name" required type="text" name="name" id="name" className="form-control"
+              />
+              <label htmlFor="name">Manufacturer Name</label>
             </div>
-          </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
       </div>
     </div>
+  </div>
+</div>
   );
 }
--- ghi/src/ModelForm.js
+++ ghi/src/ModelForm.js
@@ -61,42 +61,42 @@ export default function ModelForm() {
 
   return (
     <div>
-      <div className="my-5 container">
-        <div className="row">
-          <div className="offset-3 col-6">
-            <div className="shadow p-4 mt-4" style={{ backgroundColor: '#f2f2f2' }}>
-              <h1>Create a vehicle model</h1>
-              <form onSubmit={handleSubmit} id="create-model-form">
-                <div className="form-floating mb-3">
-                  <input
-                    value={name} onChange={handleName} placeholder="name" required type="text" name="name" id="name" className="form-control"
-                  />
-                  <label htmlFor="model_name">Model Name</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    value={pictureUrl} onChange={handlePictureUrl} placeholder="picture_url" required type="text" name="picture_url" id="picture_url" className="form-control"/>
-                  <label htmlFor="picture">Picture URL</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <select
-                    value={manufacturerId} onChange={handleManufacturerId} placeholder="manufacturer_id" required type="text" name="manufacturer_id" id="manufacturer_id" className="form-select">
-                  <option value="">Choose a manufacturer</option>
-                  {Array.isArray(manufacturers) && manufacturers.map((manufacturer) => {
-                    return manufacturer && (
-                      <option key={manufacturer.id} value={manufacturer.id}>
-                        {manufacturer.name}
-                      </option>
-                    );
-                  })}
-                  </select>
-                </div>
-                <button className="btn btn-primary">Create</button>
-              </form>
+  <div className="my-5 container">
+    <div className="row justify-content-center">
+      <div className="col-12 col-sm-8 col-md-6">
+        <div className="shadow p-4 mt-4" style={{ backgroundColor: '#f2f2f2' }}>
+          <h1>Create a vehicle model</h1>
+          <form onSubmit={handleSubmit} id="create-model-form">
+            <div className="form-floating mb-3">
+              <input
+                value={name} onChange={handleName} placeholder="name" required type="text" name="name" id="name" className="form-control"
+              />
+              <label htmlFor="model_name">Model Name</label>
             </div>
-          </div>
+            <div className="form-floating mb-3">
+              <input
+                value={pictureUrl} onChange={handlePictureUrl} placeholder="picture_url" required type="text" name="picture_url" id="picture_url" className="form-control"/>
+              <label htmlFor="picture">Picture URL</label>
+            </div>
+            <div className="form-floating mb-3">
+              <select
+                value={manufacturerId} onChange={handleManufacturerId} placeholder="manufacturer_id" required type="text" name="manufacturer_id" id="manufacturer_id" className="form-select">
+              <option value="">Choose a manufacturer</option>
+              {Array.isArray(manufacturers) && manufacturers.map((manufacturer) => {
+                return manufacturer && (
+                  <option key={manufacturer.id} value={manufacturer.id}>
+                    {manufacturer.name}
+                  </option>
+                );
+              })}
+              </select>
+            </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
       </div>
     </div>
+  </div>
+</div>
   );
 }
